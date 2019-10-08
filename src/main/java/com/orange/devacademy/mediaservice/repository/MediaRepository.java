07b2a4ac9d76b28d965a593bab43e78b50722673
Dan.Petrescu <dan.petrescu@orange.com>
2019-10-08 07:22:03 +0200

    WIP add favourites logic

--- src/main/java/com/orange/devacademy/mediaservice/repository/MediaRepository.java
+++ src/main/java/com/orange/devacademy/mediaservice/repository/MediaRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface MediaRepository extends MongoRepository<Media, String> {
 
     List<Media> findAllByTitleMatchesRegex(String title);
+
+    Set<Media> findAllById(Set<String> ids);
+
 }
--- src/main/java/com/orange/devacademy/mediaservice/service/MoviesService.java
+++ src/main/java/com/orange/devacademy/mediaservice/service/MoviesService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class MoviesService {
@@ -24,4 +25,9 @@ public class MoviesService {
     public List<Media> getMovieByTitle(String title) {
         return mediaRepository.findAllByTitleMatchesRegex("(?i).*" + title + ".*");
     }
+
+    public Set<Media> getAllById(Set<String> ids) {
+        return mediaRepository.findAllById(ids);
+    }
+
 }
